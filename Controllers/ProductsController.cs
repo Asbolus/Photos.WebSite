a8fdc6b6e0b49543fcf407dbe3a969ab6be6b309
Hao <41771449+Asbolus@users.noreply.github.com>
2019-12-17 14:51:51 +0800

    initial

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Photos.WebSite.Models;
+using Photos.WebSite.Services;
+using System.Collections.Generic;
+
+namespace Photos.WebSite.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+
+        [HttpGet("Rate")]
+        public ActionResult AddRating([FromQuery] string productId, [FromQuery] int rating)
+        {
+            ProductService.AddRating(productId, rating);
+
+            return Ok();
+        }
+    }
+}
